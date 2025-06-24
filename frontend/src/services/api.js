44059aba7b67023e64eb679338ec1ea2691397cf
Victor Belahcene Terdjemane <belahcene2@gmail.com>
2025-06-24 23:42:07 +0200

    maj api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,26 @@
+// src/services/api.js
+
 import axios from 'axios';
 
-console.log('⚙️ VITE_API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
+if (!import.meta.env.VITE_API_BASE_URL) {
+  throw new Error('VITE_API_BASE_URL non défini en build !');
+}
+
+console.log('VITE_API_BASE_URL =', import.meta.env.VITE_API_BASE_URL);
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_BASE_URL
 });
 
 API.interceptors.request.use(
-  (config) => {
+  config => {
     const token = localStorage.getItem('token');
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
-  (error) => Promise.reject(error)
+  error => Promise.reject(error)
 );
 
 export default API;
