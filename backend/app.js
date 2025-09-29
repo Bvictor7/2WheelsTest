ba3b5a165b7607ff4a685701952171a4300ded83
Bikuta <belahcene2@gmail.com>
2025-09-29 07:14:38 +0200

    Refactor: séparation app.js et server.js

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import postRoutes from './routes/posts.js';
 import authRoutes from './routes/auth.js';
-import userRoutes from './routes/users.js';
+import adminRoutes from './routes/admin.js';
+import commentRoutes from './routes/comment.js';
+import userRoutes from './routes/user.js';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/users', userRoutes);
+app.use('/uploads', express.static('uploads'));
 
 app.get('/', (req, res) => {
   res.json({ message: 'Le serveur fonctionne correctement.' });
 });
 
-app.use('/api/posts', postRoutes);
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/uploads', express.static('uploads'));
-
 export default app;
+
--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
 import connectDB from './config/db.js';
-import postRoutes from './routes/posts.js';
-import authRoutes from './routes/auth.js';
-import adminRoutes from './routes/admin.js';
-import commentRoutes from './routes/comment.js'; 
-import userRoutes from './routes/user.js';
-
-const app = express();
-connectDB();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/auth', authRoutes);
-app.use('/api/posts', postRoutes);
-app.use('/api/admin', adminRoutes);
-app.use('/api/comments', commentRoutes); 
-app.use('/api/users', userRoutes);
-app.get('/', (req, res) => {
-  res.json({ message: 'Le serveur fonctionne.' });
-});
+import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
+connectDB();
+
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
