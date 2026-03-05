c566939051ac5b5488c6c7e4368d0668987456aa
Bvictor7 <belahcene2@gmail.com>
2026-03-05 07:07:43 +0100

    fix: hide MONGO_URI from logs

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import 'dotenv/config';
 import connectDB from './config/db.js';
 import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
+
+console.log("MONGO_URI chargée :", process.env.MONGO_URI ? "OK" : "NON");
+
 connectDB();
 
 app.listen(PORT, () => {
